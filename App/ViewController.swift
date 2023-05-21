b00c4aff2e44f9254c23a59ef73ae8d126c41edf
Dave Rahardja <dave@humancode.us>
2023-05-20 17:42:44 -0700

    Initial Commit

---
+++ App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App
+//
+//  Created by Dave Rahardja on 5/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
